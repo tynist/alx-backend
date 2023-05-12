4cd81a930ab3145611664816cedc54e6850fc473
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-12 22:07:03 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -15,14 +15,12 @@ def index_range(page, page_size):
     Returns:
         tuple: A tuple containing the start and end indexes.
     """
-#     assert page > 0,  # Page number must be positive
-#     assert page_size > 0,  # Page size must be positive
-
+    
     # Calculate the start index based on the page number and page size
-    start_index = (page - 1) * page_size
+    start_idx = (page - 1) * page_size
 
     # Calculate the end index by adding the start index to the page size
-    end_index = start_index + page_size
+    end_idx = start_idx + page_size
 
     # Return a tuple containing the start and end indexes
-    return start_index, end_index
+    return start_idx, end_idx
