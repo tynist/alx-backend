40db50fc60ca367fba6c424fe6552a2017cd6679
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-13 09:17:50 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,14 +8,12 @@ from typing import List
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
-
-
+"""
+Server class to paginate a database of popular baby names.
+"""
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """
+    Cached dataset
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -23,7 +21,8 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """
+        Opens the CSV file and returns a list of rows.
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -34,11 +33,46 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0,
-        assert isinstance(page_size, int) and page_size > 0,
+        """
+        Returns a list of rows from the dataset, starting at the specified page and page size.
+
+        Args:
+            page (int, optional): The page number (1-indexed). Defaults to 1.
+            page_size (int, optional): The size of each page. Defaults to 10.
+
+        Raises:
+            AssertionError: If either of the arguments are invalid.
+
+        Returns:
+            list: A list of rows from the dataset.
+        """
+
+        assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
         if start_index >= len(dataset):
-            return []  # Return an empty list if start index is out of range
+            raise AssertionError("Start index is out of range")
         return dataset[start_index:end_index]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of start and end indexes based on pagination parameters.
+
+    Args:
+        page (int): The page number (1-indexed)
+        page_size (int): The size of each page
+
+    Returns:
+        tuple: A tuple containing the start and end indexes.
+    """
+
+    # Calculate the start index based on the page number and page size
+    start_index = (page - 1) * page_size
+
+    # Calculate the end index by adding the start index to the page size
+    end_index = start_index + page_size
+
+    return start_index, end_index
