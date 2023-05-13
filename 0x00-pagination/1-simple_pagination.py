53c11a69f548c8cad0be46a887f3b4ddbefe083f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-13 10:40:33 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,8 +40,21 @@ class Server:
             return []
         return dataset[start_idx:end_idx]
 
-    def index_range(page, page_size):
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
         """
         Returns a tuple of start & end indexes based on pagination parameters.
+
+        Args:
+            page (int): The page number (1-indexed)
+            page_size (int): The size of each page
+
+        Returns:
+            tuple: A tuple containing the start and end indexes.
         """
-        return ((page - 1) * page_size, page * page_size)
+        # Calculate the start index based on the page number and page size
+        start_idx = (page - 1) * page_size
+
+        # Calculate the end index by adding the start index to the page size
+        end_idx = start_idx + page_size
+
+        return start_idx, end_idx
