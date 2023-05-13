6c66ffe8fef721ea97620d2eadc1711ad6d46048
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-13 09:02:32 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,12 +4,6 @@ from typing import List
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
-
-
 class Server:
     """Server class to paginate a database of popular baby names.
     """
