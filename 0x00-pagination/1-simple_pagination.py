33627540191734cb4cd62c90ab8730e014112cab
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-13 09:43:44 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -9,12 +9,15 @@ from typing import Tuple
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Opens the CSV file and returns a list of rows.
+        """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -25,32 +28,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Returns a list of rows from the dataset,
-        starting at the specified page and page size.
-
-        Args:
-            page (int, optional): The page number (1-indexed). Defaults to 1.
-            page_size (int, optional): The size of each page. Defaults to 10.
-
-        Raises:
-            AssertionError: If either of the arguments are invalid.
-
-        Returns:
-            list: A list of rows from the dataset.
-        """
-
-        # Page must be an integer greater than 0
-        assert isinstance(page, int) and page > 0,
-        # Page size must be an integer greater than 0
-        assert isinstance(page_size, int) and page_size > 0,
+        assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
         if start_index >= len(dataset):
-            raise AssertionError("Start index is out of range")
+            return []  # Return an empty list if start index is out of range
         return dataset[start_index:end_index]
-
 
     def index_range(page: int, page_size: int) -> Tuple[int, int]:
         """
