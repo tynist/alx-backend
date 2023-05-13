7a48a48f792859edd6f8d3d4107fe19075e41ce0
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-13 09:13:26 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
 import csv
 import math
 from typing import List
 from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
 
 
 class Server:
@@ -24,12 +34,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        # Page must be an integer greater than 0
-        assert isinstance(page, int) and page > 0,
-        # Page size must be an integer greater than 0
-        assert isinstance(page_size, int) and page_size > 0,
+        assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
 
-    def index_range(page, page_size):
-        start_idx = (page - 1) * page_size
-        end_idx = start_idx + page_size
-        return start_idx, end_idx
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        if start_index >= len(dataset):
+            return []  # Return an empty list if start index is out of range
+        return dataset[start_index:end_index]
