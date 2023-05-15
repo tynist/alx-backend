aa5157e6191586774852ed2a84b6659e3ef2a8d1
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-15 10:01:19 +0100

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List, Tuple, Dict
+from typing import List, Dict
 
 
 class Server:
@@ -40,21 +40,20 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        dataset = self.indexed_dataset()
+        '''deletion-resilient paginate & returns a dictionary'''
         assert type(index) == int and type(page_size) == int
-        assert 0 <= index < len(dataset)
+        assert 0 <= index < len(self.indexed_dataset())
         data = []
-        idx = index + page_size
-        for i in range(index, idx):
-            if dataset.get(i):
-                data.append(dataset[i])
+        next_index = index + page_size
+        for i in range(index, next_index):
+            if self.indexed_dataset().get(i):
+                data.append(self.indexed_dataset()[i])
             else:
-                idx += 1
                 i += 1
-
+                next_index += 1
         return {
-            "index": index,
-            "next_index": idx
-            "page_size": page_size,
             "data": data,
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size
         }
