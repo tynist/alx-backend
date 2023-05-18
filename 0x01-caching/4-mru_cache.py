16a5aca06a5f80a4999b6816a2fcff53e42f9183
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-18 01:43:23 +0100

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -7,7 +7,7 @@ from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
     """
-    Class that represents a caching system using the MRU (Most Recently Used) algorithm.
+    Class that represents a caching system using the MRU (Most Recently Used).
     Inherits from the BaseCaching class.
     """
 
@@ -17,9 +17,9 @@ class MRUCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Assigns the item value to the key in the cache using the MRU algorithm.
-
-        If the number of items exceeds the maximum limit, the most recently used item will be discarded.
+        Assigns the item value to the key in the cache using the MRU alg
+        If the number of items exceeds the maximum limit,
+        the most recently used item will be discarded.
 
         Args:
             key: The key to assign the item value to.
@@ -36,8 +36,8 @@ class MRUCache(BaseCaching):
     def get(self, key):
         """
         Returns the value associated with the key in the cache.
-
-        If the key is found, it promotes the key to the most recently used position.
+        If the key is found, it promotes the key to the most
+        recently used position.
 
         Args:
             key: The key to retrieve the value for.
