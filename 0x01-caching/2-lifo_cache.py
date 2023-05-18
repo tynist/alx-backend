c8ac51a2d1b909e1950abe8823ea8b0b24213dae
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-18 01:05:14 +0100

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,6 +1,3 @@
-"""
-LIFO Caching
-"""
 from base_caching import BaseCaching
 
 
@@ -10,34 +7,34 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize the LIFOCache"""
         super().__init__()
+        self.key_indexes = []
 
     def put(self, key, item):
-        """Assigns d item value to d key in the cache using d LIFO algo
+        """Assigns the item value to the key in the cache using the LIFO algo.
         Args:
             key: The key to assign the item value to.
             item: The value to be assigned to the key.
         """
-         if key and item:
+        if key and item:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 if key in self.cache_data:
                     del self.cache_data[key]
                     self.key_indexes.remove(key)
                 else:
-                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
-                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
-                    print("DISCARD:", item_discarded)
+                    last_item = self.key_indexes.pop()
+                    del self.cache_data[last_item]
+                    print("DISCARD:", last_item)
+
             self.cache_data[key] = item
             self.key_indexes.append(key)
 
     def get(self, key):
         """Returns the value associated with the key in the cache.
-
         Args:
             key: The key to retrieve the value for.
-
         Returns:
             The value associated with the key,
-            or None if the key is None / doesn't exist in the cache
+            or None if the key is None or doesn't exist in the cache
         """
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
