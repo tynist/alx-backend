575dc7901f7ef02f82b014a1aae05a791579bc3c
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-18 01:45:38 +0100

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -7,7 +7,7 @@ from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
     """
-    Class that represents a caching system using the MRU (Most Recently Used).
+    Class that represents a caching system using the MRU algo.
     Inherits from the BaseCaching class.
     """
 
@@ -17,9 +17,9 @@ class MRUCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Assigns the item value to the key in the cache using the MRU alg
+        Assigns the item value to the key in the cache using the MRU algo.
         If the number of items exceeds the maximum limit,
-        the most recently used item will be discarded.
+        the most recently used (most_used_key) item will be discarded.
 
         Args:
             key: The key to assign the item value to.
@@ -29,22 +29,22 @@ class MRUCache(BaseCaching):
             self.cache_data[key] = item
 
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                most_recently_used_key = next(reversed(self.cache_data))
-                del self.cache_data[most_recently_used_key]
-                print(f"DISCARD: {most_recently_used_key}")
+                most_used_key = next(reversed(self.cache_data))
+                del self.cache_data[most__used_key]
+                print(f"DISCARD: {most_used_key}")
 
     def get(self, key):
         """
         Returns the value associated with the key in the cache.
-        If the key is found, it promotes the key to the most
-        recently used position.
+        If the key is found, it promotes the key to the
+        most recently used(most_used_key) position.
 
         Args:
             key: The key to retrieve the value for.
 
         Returns:
             The value associated with the key,
-            or None if the key is None or doesn't exist in the cache.
+            or None if the key is None / doesn't exist in the cache
         """
         if key is not None and key in self.cache_data:
             # Promote the key to the most recently used position
