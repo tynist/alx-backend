6d4d688572b0c14ca13a40703e6b4f2487c3db41
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-18 04:10:01 +0100

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -7,7 +7,7 @@ from base_caching import BaseCaching
 
 class LFUCache(BaseCaching):
     """
-    LFUCache class that represents a caching system using LFU  algorithm
+    LFUCache class that represents a caching system using LFU (Least Frequently Used) algorithm.
 
     Args:
         BaseCaching: The base caching class to inherit from.
@@ -18,11 +18,11 @@ class LFUCache(BaseCaching):
         Initialize the LFUCache.
         """
         super().__init__()
-        self.item_freqs = {}  # Tracks the frequency of cache items
+        self.item_frequencies = {}  # Tracks the frequency of cache items
 
     def put(self, key, item):
         """
-        Gives the item value to the key in the cache using LFU algo.
+        Add an item to the cache.
 
         Args:
             key (hashable): The key to assign the item value to.
@@ -31,34 +31,31 @@ class LFUCache(BaseCaching):
         if key is None or item is None:
             return
 
-        # Assign the item value to the key in cache_data
-		self.cache_data[key] = item
+        self.cache_data[key] = item
 
         if len(self.cache_data) > self.MAX_ITEMS:
-            # Find the key with the minimum frequency
-			item_to_discard = min(self.item_freqs, key=self.item_freqs.get)
-            self.item_freqs.pop(item_to_discard)
+            item_to_discard = min(self.item_frequencies, key=self.item_frequencies.get)
+            self.item_frequencies.pop(item_to_discard)
             self.cache_data.pop(item_to_discard)
             print(f"DISCARD: {item_to_discard}")
 
-        if key not in self.item_freqs:
-			# Initialize the frequency for a new item
-			self.item_freqs[key] = 0
+        if key not in self.item_frequencies:
+            self.item_frequencies[key] = 0  # Initialize the frequency for a new item
         else:
-            # Increment the frequency of an existing item
-			self.item_freqs[key] += 1
+            self.item_frequencies[key] += 1  # Increment the frequency of an existing item
 
     def get(self, key):
-        """Returns the value associated with the key in the cache.
+        """
+        Retrieve an item from the cache.
+
         Args:
             key (hashable): The key of the item to retrieve.
+
         Returns:
-            The value associated with the key, 
-            or None if the key is None or doesn't exist in the cache.
+            The value associated with the key, or None if the key is None or doesn't exist in the cache.
         """
         if key is None or key not in self.cache_data:
             return None
 
-        self.item_freqs[key] += 1  #Increment d frequency of accessed item
-
+        self.item_frequencies[key] += 1  # Increment the frequency of the accessed item
         return self.cache_data.get(key)
