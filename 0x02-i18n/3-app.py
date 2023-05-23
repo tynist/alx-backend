5ef529f8a4315a9268caac95979c5e108774695c
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-23 17:43:57 +0100

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -35,7 +35,7 @@ def index():
     """
     Render the index template with parametrized messages.
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
