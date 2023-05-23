7420720adb98864b6d0cadf146ffd2b52d707438
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-23 15:12:15 +0100

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Babel setup"""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -25,7 +25,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
