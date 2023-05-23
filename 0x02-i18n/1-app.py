25cc9902f99cb0da358e271de5734ef25879bd89
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-23 18:47:16 +0100

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -23,6 +23,11 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     return render_template('1-index.html')
