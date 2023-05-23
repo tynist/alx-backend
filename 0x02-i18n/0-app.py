611c04d4e319594583bfd0d3da4bf406e0b0bde8
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-23 10:26:14 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-"""
-Basic Flask app
-"""
-
+"""Basic Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """
-    hello world
-    """
+@app.route('/')
     return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
