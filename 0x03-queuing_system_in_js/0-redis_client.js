dfed26518ee55e1b15499305181a53dd29935915
Peter <ximonaka@gmail.com>
2023-06-20 21:53:36 +0000

    0-redis_client.js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+import Redis from 'redis';
+
+// Create a Redis client
+const redisClient = Redis.createClient();
+
+// Handle Redis client connection events
+redisClient.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+redisClient.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
